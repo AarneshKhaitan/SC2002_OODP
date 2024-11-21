f384b0c62928adfe5d5fc57373fc660fb88c39d4
sparsh-555 <118842745+sparsh-555@users.noreply.github.com>
2024-11-21 21:46:11 +0800

    Update PharmacistAction.java

--- src/action/PharmacistActions/PharmacistAction.java
+++ src/action/PharmacistActions/PharmacistAction.java
@@ -2,6 +2,10 @@ package action.PharmacistActions;
 
 import entity.users.User;
 
+/**
+ * Abstract base class for pharmacist-specific actions.
+ * Provides common properties or methods that are shared across various pharmacist operations.
+ */
 public interface PharmacistAction {
     void execute(User pharmacist);
 }
