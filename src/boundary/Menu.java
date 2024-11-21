2b329ea13d688df4153940188e3977e7e801b6b1
Riddham Agarwal <160809180+Riddham07@users.noreply.github.com>
2024-11-21 19:19:26 +0530

    Update Menu.java

--- src/boundary/Menu.java
+++ src/boundary/Menu.java
@@ -2,7 +2,16 @@
 package boundary;
 
 import entity.users.User;
-
+/**
+ * Represents a general menu interface in the Hospital Management System.
+ * Classes implementing this interface must define how a menu is displayed for a specific user.
+ */
 public interface Menu {
+    
+    /**
+     * Displays the menu for the given user.
+     *
+     * @param user the user for whom the menu is being displayed
+     */
     void display(User user);
 }
