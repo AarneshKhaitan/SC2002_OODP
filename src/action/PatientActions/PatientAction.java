403b701143f8a5e5d7e2ac0c35935cba4be5cb3c
sparsh-555 <118842745+sparsh-555@users.noreply.github.com>
2024-11-21 21:42:34 +0800

    Update PatientAction.java

--- src/action/PatientActions/PatientAction.java
+++ src/action/PatientActions/PatientAction.java
@@ -2,6 +2,10 @@ package action.PatientActions;
 
 import entity.users.User;
 
+/**
+ * Abstract base class for patient-specific actions.
+ * Defines common properties or methods shared among various patient actions.
+ */
 public interface PatientAction {
     void execute(User patient);
 }
